31cbaef6e70474e33ee7d86653079376ee2cc683
doerrte <dominik-1995@web.de>
2026-02-24 13:49:44 +0100

    change login page redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,17 @@
 export const dynamic = "force-dynamic";
 
 import { Suspense } from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
+  const userId = cookies().get("userId")?.value;
+
+  if (userId) {
+    redirect("/dashboard");
+  }
+
   return (
     <Suspense fallback={<div className="p-6">Lade...</div>}>
       <LoginForm />
